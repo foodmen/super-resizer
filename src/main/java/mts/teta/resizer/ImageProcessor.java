4e788d2ca488716357c603d5691d972b7ff97671
foodmen <foodmen243@yandex.ru>
2021-05-30 13:38:27 +0300

    Refactor ImageProcessor

--- src/main/java/mts/teta/resizer/ImageProcessor.java
+++ src/main/java/mts/teta/resizer/ImageProcessor.java
@@ -3,45 +3,52 @@ package mts.teta.resizer;
 import static marvinplugins.MarvinPluginCollection.*;
 
 import marvin.image.MarvinImage;
-import marvin.io.MarvinImageIO;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.builders.BufferedImageBuilder;
 import net.coobird.thumbnailator.resizers.Resizers;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Locale;
 
 public class ImageProcessor {
 
     public void processImage(BufferedImage image, ResizerApp app) throws IOException {
-        if (app.resize.isResize() || app.quality != 100 || !app.format.equals("JPEG"))
+        if (app.quality != 100)
+            image = reduceQualityImage(image, app);
+        if (app.resize.isResize())
             image = getResizedImage(image, app);
-        MarvinImage imageIn = new MarvinImage(image);
-        MarvinImage imageOut = imageIn.clone();
-        if (app.crop.isCrop()) {
-            crop(imageIn, imageOut, app.crop.getX(), app.crop.getY(), app.crop.getWidth(), app.crop.getHeight());
-        }
-        if (app.blur != 0) {
-            imageIn = imageOut;
-            gaussianBlur(imageIn, imageOut, app.blur);
-        }
-        MarvinImageIO.saveImage(imageOut, app.outputFile.getPath());
+        if (app.crop.isCrop())
+            image = getCropImage(image, app);
+        if (app.blur != 0)
+            image = getBlurImage(image, app);
+        ImageIO.write(image, app.format, app.outputFile);
     }
 
-    private BufferedImage getResizedImage(BufferedImage image, ResizerApp app) throws IOException {
-        if (!app.resize.isResize()) {
-            app.resize.setWidth(image.getWidth());
-            app.resize.setHeight(image.getHeight());
-        }
-        BufferedImage bufferedImage = (Thumbnails.of(image)
-                .size(app.resize.getWidth(), app.resize.getHeight())
+    private BufferedImage reduceQualityImage(BufferedImage image, ResizerApp app) throws IOException {
+        return Thumbnails.of(image)
                 .outputQuality(app.quality / 100)
-                .outputFormat(app.format.toLowerCase(Locale.ROOT))
-                .asBufferedImage());
+                .size(image.getWidth(), image.getHeight())
+                .asBufferedImage();
+    }
 
+    private BufferedImage getResizedImage(BufferedImage image, ResizerApp app) {
         BufferedImage destImage = new BufferedImageBuilder(app.resize.getWidth(), app.resize.getHeight()).build();
-        Resizers.BILINEAR.resize(bufferedImage, destImage);
+        Resizers.BILINEAR.resize(image, destImage);
         return destImage;
+    }
+
+    private BufferedImage getCropImage(BufferedImage image, ResizerApp app) {
+        MarvinImage imageIn = new MarvinImage(image);
+        MarvinImage imageOut = imageIn.clone();
+        crop(imageIn, imageOut, app.crop.getX(), app.crop.getY(), app.crop.getWidth(), app.crop.getHeight());
+        return imageOut.getBufferedImageNoAlpha();
+    }
+
+    private BufferedImage getBlurImage(BufferedImage image, ResizerApp app) {
+        MarvinImage imageIn = new MarvinImage(image);
+        MarvinImage imageOut = imageIn.clone();
+        gaussianBlur(imageIn, imageOut, app.blur);
+        return imageOut.getBufferedImageNoAlpha();
     }
 }
